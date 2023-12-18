c7fd0325938ba5fadb6ed95dc545cf831687dfa5
allyb3 <100173149+allyb3@users.noreply.github.com>
2023-12-18 12:33:21 -0600

    homework 6 part 1

--- 06-PythonAPI/api_keys.py
+++ 06-PythonAPI/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "65c34e528d66034377bb57186f0a7961"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "c1024a4d1b4a4f04a8d81e939057d0ca"
